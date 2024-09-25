64cfe0da1db9267f3fab60a69aa67b41260a3a84
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-25 22:20:03 +0500

    json files updated

--- CompanySupportTicketSystem.Data/IRepositories/IRepostiory.cs
+++ CompanySupportTicketSystem.Data/IRepositories/IRepostiory.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CompanySupportTicketSystem.Data.IRepositories;
 
-namespace CompanySupportTicketSystem.Data.IRepositories
+public interface IRepostiory<TEntity>
 {
-    internal class IRepostiory
-    {
-    }
+    public Task<bool> InsertAsync(TEntity entity);
+    public Task<bool> UpdateAsync(TEntity entity);
+    public Task<bool> DeleteByIdAsync(int id);
+    public Task<TEntity> RetriewByIdAsync(int id);
+    public Task<List<TEntity>> GetAllAsync();
 }
