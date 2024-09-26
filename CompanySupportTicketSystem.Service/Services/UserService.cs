9bdbcb8103266e673ed4e2f68bd06c20a6ac88ae
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-26 14:55:05 +0500

    UserService has written

---
+++ CompanySupportTicketSystem.Service/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using CompanySupportTicketSystem.Domain.Entities;
+using CompanySupportTicketSystem.Service.DTOs.Users;
+using CompanySupportTicketSystem.Service.Interfaces;
+
+namespace CompanySupportTicketSystem.Service.Services;
+
+public class UserService : IUserService
+{
+    public Task<bool> AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserForResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserForResultDto> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(long id, UserForUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
