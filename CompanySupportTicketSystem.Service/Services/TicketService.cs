defad1ad7fdb6b8e5407e8852df5ae6461922c0d
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-26 15:06:21 +0500

    userservice has written

---
+++ CompanySupportTicketSystem.Service/Services/TicketService.cs
@@ -0,0 +1,38 @@
+﻿using CompanySupportTicketSystem.Domain.Entities;
+using CompanySupportTicketSystem.Service.DTOs.Tickets;
+using CompanySupportTicketSystem.Service.Interfaces;
+
+namespace CompanySupportTicketSystem.Service.Services;
+
+public class TicketService : ITicketService
+{
+    public Task<bool> AddAsync(Ticket ticket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CancelTicketAsync(int ticketId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TicketForResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TicketForResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TicketForUpdateDto> UpdateAsync(long id, TicketForUpdateDto ticket)
+    {
+        throw new NotImplementedException();
+    }
+}
--- CompanySupportTicketSystem.Service/Services/UserService.cs
+++ CompanySupportTicketSystem.Service/Services/UserService.cs
@@ -1,33 +1,93 @@
 ﻿using CompanySupportTicketSystem.Domain.Entities;
 using CompanySupportTicketSystem.Service.DTOs.Users;
 using CompanySupportTicketSystem.Service.Interfaces;
+using CompanySupportTicketSystem.Data.IRepositories;
+using CompanySupportTicketSystem.Data.Repositories;
+using CompanySupportTicketSystem.Service.Exceptions;
+
 
 namespace CompanySupportTicketSystem.Service.Services;
 
 public class UserService : IUserService
 {
-    public Task<bool> AddAsync(User user)
+    IRepostiory<User> userRespository = new Repository<User>();
+
+    public async Task<bool> AddAsync(User user)
     {
-        throw new NotImplementedException();
+        var users = await this.userRespository.RetrievAllAsync();
+        if (users.Any(u => u.Email.Equals(user.Email, StringComparison.OrdinalIgnoreCase)))
+            throw new TicketExceptions(409, "User already exists");
+        users.Add(user);
+        return true;
     }
 
-    public Task<bool> DeleteByIdAsync(int id)
+    public async Task<bool> DeleteByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var deleteResponse = await this.userRespository.DeleteByIdAsync(id);
+        if (deleteResponse)
+            return true;
+        throw new TicketExceptions(404, "User not found");
     }
 
-    public Task<IEnumerable<UserForResultDto>> GetAllAsync()
+    public async Task<IEnumerable<UserForResultDto>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var users = await this.userRespository.RetrievAllAsync();
+        if (!users.Any())
+            throw new TicketExceptions(404, "Users not found");
+
+        var mappedUsers = users.Select(u => new UserForResultDto(){
+            FirstName = u.FirstName,
+            LastName = u.LastName,
+            Email = u.Email,
+            PhoneNumber = u.PhoneNumber,
+            Gender = u.Gender,
+            
+        }).ToList();
+
+        return mappedUsers;
     }
 
-    public Task<UserForResultDto> GetByIdAsync(int id)
+    public async Task<UserForResultDto> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await this.userRespository.RetrievByIdAsync(id);
+        if (user is null)
+            throw new TicketExceptions(404, "User not found");
+        var mappedUser = new UserForResultDto()
+        {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            Gender = user.Gender,
+        };
+
+        return mappedUser;
     }
 
-    public Task<bool> UpdateAsync(long id, UserForUpdateDto dto)
+    public async Task<bool> UpdateAsync(long id, UserForUpdateDto dto)
     {
-        throw new NotImplementedException();
+        var userUpdate = await this.userRespository.RetrievByIdAsync(id);
+        if (userUpdate is null)
+            throw new TicketExceptions(404, "User not found");
+
+        var mappedUser = new User()
+        {
+            Id = userUpdate.Id,
+            FirstName = userUpdate.FirstName,
+            LastName = userUpdate.LastName,
+            Email = userUpdate.Email,
+            PhoneNumber = userUpdate.PhoneNumber,
+            Gender = userUpdate.Gender,
+            Password = userUpdate.Password,
+            Address = userUpdate.Address,
+            DateOfBirth = userUpdate.DateOfBirth,
+            CreatedDate = userUpdate.CreatedDate,
+            PaymentMethod = userUpdate.PaymentMethod,
+            UpdatedDate = userUpdate.UpdatedDate,
+
+        };
+
+        this.userRespository.UpdateAsync(mappedUser);
+        return true;
     }
 }
