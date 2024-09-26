c24d85d5717e1792733175ce2c821a4c04dd96f8
muhammadmirzo-pixel <140395371+muhammadmirzo-pixel@users.noreply.github.com>
2024-09-26 16:05:09 +0500

    Added ORDER_PATH to repository

--- CompanySupportTicketSystem.Domain/Configurations/DatabasePath.cs
+++ CompanySupportTicketSystem.Domain/Configurations/DatabasePath.cs
@@ -6,4 +6,5 @@ public class DatabasePath
     public const string COMPANY_PATH = "../../../../CompanySupportTicketSystem.Data/DataBases/Company.json";
     public const string COMPANY_CATEGORY = "../../../../CompanySupportTicketSystem.Data/DataBases/CompanyCategory.json";
     public const string TICKET_PATH = "../../../../CompanySupportTicketSystem.Data/DataBases/Ticket.json";
+    public const string ORDER_PATH = "../../../../CompanySupportTicketSystem.Data/DataBases/Order.json";
 }
