5fd3e4a9542259b176cbe9ba9a2436b74dd2eff2
muhammadmirzo-pixel <140395371+muhammadmirzo-pixel@users.noreply.github.com>
2024-09-28 16:44:00 +0500

    program.cs ichi bo'shatildi

--- CompanySupportTicketSystem.Presentation/Program.cs
+++ CompanySupportTicketSystem.Presentation/Program.cs
@@ -1,50 +1,10 @@
-﻿/*{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}*/
+﻿namespace CompanySupportTicketSystem.Presentation;
 
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using CompanySupportTicketSystem.Service.Services;
-using CompanySupportTicketSystem.Service.DTOs.Orders;
-
-namespace CompanySupportTicketSystem.Presentation;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        var orderService = new OrderService();
-        var orderDto = new OrderForCreationDto
-        {
-            Seat = "1A",
-            UserId = 123, // mavjud UserId
-            TicketId = 456 // mavjud TicketId
-        };
-
-        // Order qo'shish
-        bool result = await orderService.AddAsync(orderDto);
-
-        // Order.json faylini tekshirish
-        var filePath = "D:\\Imtihon\\Company Support Ticket System\\CompanySupportTicketSystem.Data\\DataBases\\Order.json"; // Faylning to'liq yo'lini kiriting
-        if (File.Exists(filePath))
-        {
-            var jsonContent = await File.ReadAllTextAsync(filePath);
-            Console.WriteLine("Faylning mazmuni:");
-            Console.WriteLine(jsonContent);
-        }
-        else
-        {
-            Console.WriteLine("Order.json fayli topilmadi.");
-        }
-
-
-       
+        Console.WriteLine("Hello, World!");
     }
 }
