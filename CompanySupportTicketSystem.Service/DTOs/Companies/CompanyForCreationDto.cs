94c4f23957e403cbfe327c6a6845becb083efbd4
Muhammadmirzo <140395371+muhammadmirzo-pixel@users.noreply.github.com>
2024-10-16 00:20:34 +0500

    Update CompanyForCreationDto.cs
    Signed-off-by: Muhammadmirzo <140395371+muhammadmirzo-pixel@users.noreply.github.com>

--- CompanySupportTicketSystem.Service/DTOs/Companies/CompanyForCreationDto.cs
+++ CompanySupportTicketSystem.Service/DTOs/Companies/CompanyForCreationDto.cs
@@ -9,8 +9,6 @@ public class CompanyForCreationDto
     public string Address { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
-
     public bool IsActive { get; set; }
-
     public decimal Rating { get; set; }
 }
